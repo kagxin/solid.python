7f941c631d7cba769859004456fd22545a479a13
kangxin <k506855660@163.com>
2019-06-25 17:09:29 +0800

    Update 5.dip.py
    添加中文注释

--- 5.dip.py
+++ 5.dip.py
@@ -7,6 +7,15 @@ B. Abstractions should not depend on details. Details should depend upon abstrac
 There comes a point in software development where our app will be largely composed of modules. 
 When this happens, we have to clear things up by using dependency injection. 
 High-level components depending on low-level components to function.
+
+- 依赖反转原则
+依赖关系应该是抽象的，而不是具体的 (依赖于抽象而不是一个实例 wiki)
+A.高级模块不应该依赖于低级模块。两者都应该依赖于抽象。
+B.抽象不应该依赖于细节。细节应该依赖于抽象。
+
+在软件开发中，我们的应用程序将主要由模块组成。
+当这种情况发生时，我们必须使用依赖注入来清理。
+高级组件取决于要运行的低级组件。
 """
 
 class XMLHttpService(XMLHttpRequestService):
@@ -31,6 +40,15 @@ If we were to change the Http connection service, maybe we want to connect to th
 We will painstakingly have to move through all the instances of Http to edit the code and this violates the OCP principle.
 
 The Http class should care less the type of Http service you are using. We make a Connection interface:
+
+- 这里，Http是高级组件，而HttpService是低级组件。
+这种设计违反了DIP A:高级模块不应该依赖于低级模块。它应该依赖于它的抽象。
+
+Http类被迫依赖于XMLHttpService类。
+如果要更改Http连接服务，可能需要通过cURL甚至Mock Http服务连接到internet。
+我们将费力地遍历所有Http实例来编辑代码，这违反了OCP原则。
+
+Http类不应该太关心您正在使用的Http服务的类型。我们做一个连接接口:
 """
 
 class Connection:
@@ -56,6 +74,10 @@ So now, no matter the type of Http connection service passed to Http it can easi
 without bothering to know the type of network connection.
 
 We can now re-implement our XMLHttpService class to implement the Connection interface:
+- 所以现在，无论传递给Http的Http连接服务的类型是什么，它都可以轻松地连接到网络
+不需要知道网络连接的类型。
+
+我们现在可以重新实现XMLHttpService类来实现连接接口:
 """
 
 class XMLHttpService(Connection):
@@ -67,6 +89,7 @@ class XMLHttpService(Connection):
 
 """
 We can create many Http Connection types and pass it to our Http class without any fuss about errors.
+- 我们可以创建许多Http连接类型并将其传递给我们的Http类，而无需担心错误。
 """
 class NodeHttpService(Connection):
     def request(self, url: str, options:dict):
@@ -83,4 +106,10 @@ the Http service types(low level modules) in turn, depends on the Connection int
 
 Also, this DIP will force us not to violate the Liskov Substitution Principle: 
 The Connection types Node-XML-MockHttpService are substitutable for their parent type Connection.
+- 现在，我们可以看到高级模块和低级模块都依赖于抽象。
+Http类(高级模块)依赖于连接接口(抽象)和
+Http服务类型(低层模块)反过来又依赖于连接接口(抽象)。
+
+此外，这种下降将迫使我们不违反里氏替换原则:
+连接类型Node-XML-MockHttpService可以替代它们的父类型连接。
 """
